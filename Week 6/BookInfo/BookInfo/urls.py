2ca25e10f457b976a13bd54ef0fe2009c8bac8c1
Jeffin joffy <114131523+Apetun@users.noreply.github.com>
2025-04-07 11:55:33 +0530

    Test

--- Week 6/BookInfo/BookInfo/urls.py
+++ Week 6/BookInfo/BookInfo/urls.py
@@ -1,24 +1,10 @@
-"""
-URL configuration for BookInfo project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import home, metadata, reviews, publisher
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('bookinfo.urls')),  # Routes the root URL to bookinfo app
+    path('', home, name='home'),
+    path('metadata/', metadata, name='metadata'),
+    path('reviews/', reviews, name='reviews'),
+    path('publisher/', publisher, name='publisher'),
 ]
 
